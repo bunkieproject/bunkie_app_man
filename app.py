10aba458c53937c5560734e335343745121c00cb
ituitis-canb18 <canbatuhan.official@gmail.com>
2023-01-01 13:35:13 +0300

    BUN-89-90 : talking with backend

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, Response
+import requests
+import json
+import os
+
+SERVER = 'https://bunkie-backend-foja2uwzca-ey.a.run.app'
+TOKEN = ""
+IMAGE_FOLDER = os.path.join('static', 'img')
+
+app = Flask(__name__)
+app.config['SERVER'] = SERVER
+app.config['TOKEN'] = TOKEN
+app.config['IMAGE_FOLDER'] = IMAGE_FOLDER
+
+@app.route('/')
+def index():
+    icon_filename = os.path.join(app.config['IMAGE_FOLDER'], 'icon.png')
+    logo_filename = os.path.join(app.config['IMAGE_FOLDER'], 'logo.png')
+    return render_template("index.html", icon=icon_filename, logo=logo_filename)
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['admin-email']
+    password = request.form['admin-pw']
+    headers = {'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json; charset=UTF-8'}
+    response = requests.post(
+        app.config['SERVER']+'/users/admin/login',
+        json={"username_or_email": email, "password": password},
+        headers=headers)
+    if response.status_code == 200:
+        app.config['TOKEN'] = dict(json.loads(response.text))['token']
+        icon_filename = os.path.join(app.config['IMAGE_FOLDER'], 'icon.png')
+        logo_filename = os.path.join(app.config['IMAGE_FOLDER'], 'logo.png')
+        return render_template("console.html", icon=icon_filename, logo=logo_filename)
+    else:
+        return None, response.status_code, headers
+
+if __name__ == "__main__":
+    app.run()
