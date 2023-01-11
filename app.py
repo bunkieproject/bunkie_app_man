ee6c106d68f08bd75c2933b4e94e6d03d3ca3044
kutaygallo <kutaygallo@gmail.com>
2023-01-11 18:18:36 +0300

    Final draft of web app.

--- app.py
+++ app.py
@@ -58,5 +58,77 @@ def get_user():
 
     return render_template("console.html", icon=icon_filename, logo=logo_filename, user_id="")
 
+@app.route('/warn_user', methods=['POST'])
+def warn_user():
+    user_id = request.form['userid']
+    headers = {'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json; charset=UTF-8'}
+
+    response = requests.post(
+        app.config['SERVER'] + '/users/admin/warn_user',
+        json={"token": app.config['TOKEN'], "user_id": user_id},
+        headers=headers)
+
+    if response.status_code == 200:
+        icon_filename = os.path.join(app.config['IMAGE_FOLDER'], 'icon.png')
+        logo_filename = os.path.join(app.config['IMAGE_FOLDER'], 'logo.png')
+        return render_template("console.html", icon=icon_filename, logo=logo_filename, user_id="")
+    else:
+        return None, response.status_code, headers
+
+@app.route('/ban_user', methods=['POST'])
+def ban_user():
+    user_id = request.form['userid']
+    headers = {'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json; charset=UTF-8'}
+
+    response = requests.post(
+        app.config['SERVER'] + '/users/admin/ban_user',
+        json={"token": app.config['TOKEN'], "user_id": user_id},
+        headers=headers)
+
+    if response.status_code == 200:
+        icon_filename = os.path.join(app.config['IMAGE_FOLDER'], 'icon.png')
+        logo_filename = os.path.join(app.config['IMAGE_FOLDER'], 'logo.png')
+        return render_template("console.html", icon=icon_filename, logo=logo_filename, user_id="")
+    else:
+        return None, response.status_code, headers
+
+@app.route('/unban_user', methods=['POST'])
+def unban_user():
+    user_id = request.form['userid']
+    headers = {'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json; charset=UTF-8'}
+
+    response = requests.post(
+        app.config['SERVER'] + '/users/admin/unban_user',
+        json={"token": app.config['TOKEN'], "user_id": user_id},
+        headers=headers)
+
+    if response.status_code == 200:
+        icon_filename = os.path.join(app.config['IMAGE_FOLDER'], 'icon.png')
+        logo_filename = os.path.join(app.config['IMAGE_FOLDER'], 'logo.png')
+        return render_template("console.html", icon=icon_filename, logo=logo_filename, user_id="")
+    else:
+        return None, response.status_code, headers
+
+@app.route('/delete_user', methods=['POST'])
+def delete_user():
+    user_id = request.form['userid']
+    headers = {'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json; charset=UTF-8'}
+
+    response = requests.post(
+        app.config['SERVER'] + '/users/admin/delete_user',
+        json={"token": app.config['TOKEN'], "user_id": user_id},
+        headers=headers)
+
+    if response.status_code == 200:
+        icon_filename = os.path.join(app.config['IMAGE_FOLDER'], 'icon.png')
+        logo_filename = os.path.join(app.config['IMAGE_FOLDER'], 'logo.png')
+        return render_template("console.html", icon=icon_filename, logo=logo_filename, user_id="")
+    else:
+        return None, response.status_code, headers
+
 if __name__ == "__main__":
     app.run()
